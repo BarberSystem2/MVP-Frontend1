b2baa84ce2e1cef798a23c74b642b2f9db21d82f
AC25-C <151057176+AC25-C@users.noreply.github.com>
2025-05-07 14:20:20 +0200

    overview

--- .idea/src/js/app.js
+++ .idea/src/js/app.js
@@ -1,5 +1,53 @@
-const API_URL = "http://localhost:8080/bookings/getbookings"; // ← Udskift med din rigtige API-URL
+ // ← Udskift med din rigtige API-URL
 
+document.addEventListener("DOMContentLoaded", () => {
+    const employeeSelect = document.getElementById("employee");
+
+    fetch("http://localhost:8080/employee/allEmployees") // ← Brug din faktiske API URL
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Kunne ikke hente medarbejdere");
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(employee => {
+                const option = document.createElement("option");
+                option.value = employee.employeeId;
+                option.textContent = employee.employeeName;
+                employeeSelect.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error("Fejl ved hentning af medarbejderdata:", error);
+        });
+});
+
+ document.addEventListener("DOMContentLoaded", () => {
+     const serviceTypeSelect = document.getElementById("service");
+
+     fetch("http://localhost:8080/serviceType/getAll")
+         .then(response => {
+             if (!response.ok) {
+                 throw new Error("Kunne ikke hente services");
+             }
+             return response.json();
+         })
+         .then(data => {
+             data.forEach(servicetype =>{
+                 const option = document.createElement("option");
+                 option.value = servicetype.serviceTypeId;
+                 option.textContent = servicetype.serviceType +
+                 " Service " + servicetype.serviceTypePrice + ",- Kr " + servicetype.estimatedTimer + " min.";
+                 serviceTypeSelect.appendChild(option);
+             });
+         })
+         .catch(error =>{
+             console.error("Fejl", error)
+         });
+ });
+
+ const API_URL = "http://localhost:8080/bookings/getbookings";
 document.addEventListener("DOMContentLoaded", () => {
     const tbody = document.querySelector("#bookingTable tbody");
 
